1381b53a17fa2fbe3c6285cf66634ba841fbb93a
Joshua Clinton <Joshuac@ami.com>
2020-04-19 15:37:21 -0400

    Added border and dropdown button Created the nav menu with a border started work on a dropdown list for the different workouts still not showing the data from the file.

--- app.js
+++ app.js
@@ -6,3 +6,58 @@ let today = new Date()
 let formatDate = today.toDateString()
 let selectElement = document.getElementById('date')
 selectElement.innerHTML = formatDate
+
+var workoutChoose = document.querySelector('select');
+var wodDisplay = document.querySelector('main');
+
+workoutChoose.onchange = function() {
+    const workout = workoutChoose.value;
+    updateDisplay(workout);
+};
+
+/*<!-- Old version using XHR (XMLHttpRequest)
+function updateDisplay(workout) {
+    workout = workout.replace(" ", "");
+    workout = workout.toLowerCase();
+    let url = workout + '.txt';
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'text';
+    request.onload = function() {
+        wodDisplay.textContent = request.response;
+    };
+    request.send();
+};
+-->*/
+
+//New version using Fetch API
+function updateDisplay(workout) {
+    workout = workout.replace(" ", "");
+    workout = workout.toLowerCase();
+    let url = workout + '.txt';
+    fetch(url).then(function(response) {
+        response.text().then(function(text) {
+            wodDisplay.textContent = text;
+        });
+    });
+};
+updateDisplay('Workout 1');
+workoutChoose.value = 'Workout 1';
+/*
+function myFunction() {
+    document.getElementById('myDropdown').classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+*/
